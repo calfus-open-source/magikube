843040c3c3cc40221a062288d2345db6d5d9d26b
Aditya Gupta <agupta@tradefull.com>
2024-06-26 20:16:03 +0530

    initial commit for nodegroup cluster creation

--- src/core/terraform-project.ts
+++ src/core/terraform-project.ts
@@ -3,6 +3,7 @@ import BaseCommand from '../commands/base.js';
 import EKSFargateProject from './aws/aws-eks-fargate.js';
 import BaseProject from './base-project.js';
 import AWSK8SProject from './aws/aws-k8s.js';
+import EKSNodeGrpClusterProject from './aws/aws-nodegrp-cluster.js';
 
 export default abstract class TerraformProject {    
     static async getProject(command: BaseCommand): Promise<BaseProject | null> {
@@ -14,6 +15,9 @@ export default abstract class TerraformProject {
 
             if (config.cluster_type === 'k8s') 
                 return new AWSK8SProject(command, config);
+
+            if (config.cluster_type === 'eks-nodegroup')
+                return new EKSNodeGrpClusterProject(command, config);
             command.error(`Cloud provider '${config.cloud_provider}' and cluster type '${config.cluster_type}' not supported`);
         }   
         
