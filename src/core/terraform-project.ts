1b129f6db01bfb2fb2ddb9173ad52e040ba1b2bf
Neel Kamal Shah <147086999+neelshah19@users.noreply.github.com>
2024-06-28 16:51:13 -0700

    K8s accessibility (#17)
    * added k8s-accessiblity and setup
    * modified destroying command
    * modularized ingress controller
    * added conditions for handling various clusters

--- src/core/terraform-project.ts
+++ src/core/terraform-project.ts
@@ -1,11 +1,11 @@
 import SystemConfig from '../config/system.js';
 import BaseCommand from '../commands/base.js';
 import EKSFargateProject from './aws/aws-eks-fargate.js';
-import BaseProject from './base-project.js';
 import AWSK8SProject from './aws/aws-k8s.js';
+import AWSProject from './aws/aws-project.js';
 
 export default abstract class TerraformProject {    
-    static async getProject(command: BaseCommand): Promise<BaseProject | null> {
+    static async getProject(command: BaseCommand): Promise<AWSProject | null> {
         const config = SystemConfig.getInstance().getConfig();
 
         if (config.cloud_provider === 'aws') {
@@ -19,4 +19,16 @@ export default abstract class TerraformProject {
         
         command.error(`Cloud provider '${config.cloud_provider}' not supported`);
     }       
+    static async mockGetProject(command: BaseCommand, config: any): Promise<AWSProject | null> {
+        if (config.cloud_provider === 'aws') {
+            if (config.cluster_type === 'eks-fargate') 
+                return new EKSFargateProject(command, config);
+
+            if (config.cluster_type === 'k8s') 
+                return new AWSK8SProject(command, config);
+            command.error(`Cloud provider '${config.cloud_provider}' and cluster type '${config.cluster_type}' not supported`);
+        }   
+        
+        command.error(`Cloud provider '${config.cloud_provider}' not supported`);
+    }
 }
