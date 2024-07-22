24c568e62920db01405ad45ab9edb0fbf4bfa871
Aditya Gupta <agupta@tradefull.com>
2024-07-22 16:15:42 +0530

    adding missing module files

---
+++ src/core/code-repository/common-repository.ts
@@ -0,0 +1,32 @@
+import BaseProject from '../base-project.js';
+import { join } from 'path';
+
+export default class repositoryProject extends BaseProject {
+  async createrepository(path: any, name: any): Promise<void> {
+    this.projectPath = join(path, name);
+    
+    if (this.config.source_code_repository === "codecommit") {
+       this.createFile(
+         "main.tf",
+         "../templates/code-repository/aws/main.tf.liquid",
+         "./modules/code-repository"
+       );
+       this.createFile(
+         "variables.tf",
+         "../templates/code-repository/aws/variables.tf.liquid",
+         "./modules/code-repository"
+       );
+     } else if (this.config.source_code_repository === "github") {
+         this.createFile(
+             "main.tf",
+             "../templates/code-repository/github/main.tf.liquid",
+             "./modules/code-repository",
+         );
+         this.createFile(
+             "variables.tf",
+             "../templates/code-repository/github/variables.tf.liquid",
+             "./modules/code-repository",
+         );
+     }
+  }
+}
