9c81d1b88abce885fd61aa9bf0f04e78ffb06b87
RohitBhosale204 <Rohit.Bhosale@calfus.com>
2025-01-27 10:48:16 +0530

    quick fix for destroy command bug

--- src/core/utils/statusUpdater-utils.ts
+++ src/core/utils/statusUpdater-utils.ts
@@ -80,15 +80,13 @@ export function updateStatusFile(
 
 
 // To reade the status.json file
-export function readStatusFile(projectConfig:any){
-  const statusFilePath = path.join(
-    process.cwd(),
-    // projectConfig.command === "module"
-    //   ? projectConfig.project_name
-    //   : 
-     projectConfig.project_name,
-    "status.json"
-  );
+export function readStatusFile(projectConfig:any, command?:any){
+  let statusFilePath;
+  if(command === 'module'){
+      statusFilePath = path.join(process.cwd(), "status.json");
+  }else{
+      statusFilePath = path.join(process.cwd(),projectConfig.project_name, "status.json");
+  }
   if (!fs.existsSync(statusFilePath)) {
   return null;
   }
